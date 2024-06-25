6d9bd004c28dc06471745c450acfc05adce156fd
lilien86 <lilienauger.86@gmail.com>
2024-06-25 12:04:33 +0200

    modif print_data

--- philo/src/print_data.c
+++ philo/src/print_data.c
@@ -6,7 +6,7 @@
 /*   By: lauger <lauger@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/21 14:00:31 by lauger            #+#    #+#             */
-/*   Updated: 2024/06/24 14:16:49 by lauger           ###   ########.fr       */
+/*   Updated: 2024/06/25 09:14:18 by lauger           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,25 +17,25 @@ void	print_data_state(t_data *data, p_threads *thread)
 	if (!data || !thread)
 		return ;
 	pthread_mutex_lock(&data->mutex_print);
+	long long	elapsed_time;
+	
+	elapsed_time = get_elapsed_time(data->start_time);
 	printf(WHITE "data-thread id:" YELLOW " %d" GREEN" | ", thread->id);
 	printf(WHITE"--State: ");
 	if (thread->state == THINKING)
 	{
 		printf(YELLOW "Thinking" GREEN"  | ");
-		printf(WHITE "Thinking time: " YELLOW "%lld\n",
-			get_elapsed_time(thread->last_eat_time));
+		printf(WHITE "Thinking time: " YELLOW "%lld\n", elapsed_time);
 	}
 	else if (thread->state == SLEEPING)
 	{
 		printf(YELLOW "Sleeping" GREEN"  | ");
-		printf(WHITE "Sleeping time: " YELLOW "%lld\n",
-			get_elapsed_time(thread->last_eat_time));
+		printf(WHITE "Sleeping time: " YELLOW "%lld\n", elapsed_time);
 	}
 	else if (thread->state == EATING)
 	{
 		printf(YELLOW "Eating" GREEN"    | ");
-		printf(WHITE "Eating   time: " YELLOW "%lld\n",
-			get_elapsed_time(thread->last_eat_time));
+		printf(WHITE "Eating   time: " YELLOW "%lld\n", elapsed_time);
 	}
 	pthread_mutex_unlock(&data->mutex_print);
 }
@@ -44,10 +44,12 @@ void	print_data_action(t_data *data, p_threads *thread, char *action)
 {
 	if (!data || !thread || !action)
 		return ;
+	long long	elapsed_time;
+	
+	elapsed_time = get_elapsed_time(data->start_time);
 	pthread_mutex_lock(&data->mutex_print);
 	printf(WHITE "data-thread id:" YELLOW " %d" GREEN" | ", thread->id);
 	printf(MAGENTA "--%s " GREEN"| ", action);
-	printf(WHITE "Get fork time: " YELLOW "%lld\n",
-		get_elapsed_time(thread->last_eat_time));
+	printf(WHITE "Get fork time: " YELLOW "%lld\n", elapsed_time);
 	pthread_mutex_unlock(&data->mutex_print);
 }
